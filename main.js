8c19bab567e34aefa24a3a1b50b1b00b2cba25d0
swastik-d-coder <102283282+swastik-d-coder@users.noreply.github.com>
2022-03-30 19:00:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX=0;
+rightWrist=0;
+difference=0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -15,5 +19,25 @@ function modelLoaded()
  }
 
  function gotPoses(results) {
+    if(results.lenght > 0) {
+console.log(results);
+
+leftWristX = results[0].pose.leftWrist.x;
+rightWristX=results[0].pose.rightWrist.x;
+
+
+difference = floor(leftWristX - rightWristX);
+  
+console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference );
     
+    }
+}
+
+function draw() {
+    document.getElementById("font-size") .innerHTML = "Size of the text will be = " + difference + "px";
+  
+    background("#6C91C2");
+ textSize(difference);
+ fill("#000000");
+ text('Swastik', 50, 400);
 }
